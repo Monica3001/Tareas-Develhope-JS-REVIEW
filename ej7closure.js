18d46b5947abeb702140514bd854c48f6737572d
Monica Guerra <monik_flute@hotmail.com>
2023-12-27 18:07:46 +0100

    ejercicio 7 closures

---
+++ ej7closure.js
@@ -0,0 +1,23 @@
+// Try to write your first closure. Write a function called printName and 
+// declare inside it a variable called helloName, the value of this variable
+//  will be: "Hello John". Also declare a function named inner and return the 
+//  helloName variable. The purpose is printing "Hello John" in the console.
+
+// Starting from the previous exercise, 
+// are you able to print the value of helloName, after 1 second?
+
+function printName() {
+    helloName="hello John"; 
+
+ function inner() {
+    
+     setTimeout(() => {
+      console.log(helloName)
+       } ,1000);
+ }
+ inner()
+ }
+ printName();
+
+
+ 
