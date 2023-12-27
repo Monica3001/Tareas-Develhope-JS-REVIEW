a505666081bfd9991fc7e2c9e679d0bc3ae74455
Monica Guerra <monik_flute@hotmail.com>
2023-12-27 18:01:33 +0100

    ejercicio 5 object copying

---
+++ objc.js
@@ -0,0 +1,26 @@
+// In this exercise we have created two objects: 
+// person1 e person2. The object person2 has been assigned to
+//  the object person1. You should modify the property firstName
+//   of the object person2 in "Simon".
+
+// Write a comment explaining why, by modifying the object person2,
+//  also the object person1 would be modified.
+// Modify the property `firstName` of the `person2` in "Simon".
+
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
+
+const person2 = person1;
+person2.firstName= "simon";
+
+console.log(person1);
+console.log(person2);
+
+// comment: 
+// Una caracteristica de los objetos es que estos se guardan y modifican,
+// usando referencias (firstName, LastName y age.)
+// al colocar person2 =person1, se crean dos variables guardando la 
+// misma referencia en vez de duplicarse.
