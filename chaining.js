bb2bc564d15bfd740a6db0c015871d4fd53be9d3
Monica Guerra <monik_flute@hotmail.com>
2023-12-27 18:16:29 +0100

    ejercicio 10 chaining

---
+++ chaining.js
@@ -0,0 +1,15 @@
+// In this example we are trying to check if a value
+//  for the city property is defined. However, 
+//  the code can be simplified. How can we do that?
+
+const order = {};
+
+if 
+(order && order.customer && order.customer.address && !order.customer.address.city) {
+  console.log('City is required');
+}
+
+
+if (order.customer.address.city === undefined) {
+  console.log('City is required');
+}
