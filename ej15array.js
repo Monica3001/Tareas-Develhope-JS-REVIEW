80569fe2c45e1b96bb1e63d9d21a6da1b2439ff1
Monica Guerra <monik_flute@hotmail.com>
2023-12-27 18:26:18 +0100

    ejercicio 15 array

---
+++ ej15array.js
@@ -0,0 +1,23 @@
+//Create a function called sortPeopleByAge that returns the people in ascending order by age.
+
+function sortPeopleByAge(people) {
+    const sortingByAge = Object.values(people).sort((a, b) => { 
+        return a.age - b.age })
+    
+    console.log(sortingByAge)
+}
+
+const people = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const sortingByAge = sortPeopleByAge(people)
